232f92dc5661e66e7f3c415f1a5ff0c402c641fc
AMENAB12 <lalatube71@gmail.com>
2022-12-20 18:20:25 -0800

    my first commit

---
+++ Second Phase/JS/practicing/java.js
@@ -0,0 +1,20 @@
+/**Function */
+var n = 0;
+console.log(av)
+function adder(x, y) {
+    var c;
+    c = x + y;
+    return c;
+
+}
+var added = adder (8, 5)
+console.log(added)
+function average(x, y) {
+var av = 0;
+av = adder(x, y)/2;
+return av;
+
+}
+var k = average(100, 50)
+console.log(k)
+/**Arrow Function */
