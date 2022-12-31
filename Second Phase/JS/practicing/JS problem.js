e3b32d6c0ef46dc34c3f362d132280de2829326e
AMENAB12 <lalatube71@gmail.com>
2022-12-30 18:42:15 -0800

    Arrange my code

--- Second Phase/JS/practicing/JS problem.js
+++ Second Phase/JS/practicing/JS problem.js
@@ -1,3 +1,4 @@
+     /**Function Practice Questions */
 /**Q1 - Define a simple function named myFirst that prints the word "Hello" on the console.
 ○ First define the function
 ○ Then call the function*/
@@ -8,78 +9,62 @@ myFirst();
 /**Q2 - Define a function called mySecond that takes a parameter and prints the parameter on console
 ○ Feel free to give any value as a parameter in your function.*/
 function mySecond (x, y){
-var d;
-d = x - y;
-return d;
+return x - y;
 }
-var sub = mySecond(9, 5)
-console.log(sub)
+console.log(mySecond(9, 5))
 /**Q3 ● Define a function called myThird that takes a parameter and prints the parameter on the console. But, it uses mySecond function to print the parameter on the console.*/
 function myThird (x, y){
-    var per;
-    per = mySecond(x, y) * 6;
-    return per;
+    return mySecond(x, y) * 6;
 }
-var k = myThird(6, 2);
-console.log(k)
+console.log(myThird(6, 2))
 /**● Write a function named myFourth that takes an array as a parameter and prints only the first value of the array on the console. Remember, this question is here to explain the concept of function delegation in JavaScript. */
-function myFourth (){
-   var d = [myFirst, mySecond, myThird]
-    return d;
+
+function myFourth (x){
+   var t = [x];
+   x++;
+    return t[0];
 }
-var b = myFourth()
-console.log(b);
+console.log(myFourth(5,2,2));
 /** ● Write a function named myFifth that takes an array with two numbers in it as a parameter and prints the sum of the two numbers on console*/
-function myFifth (){
-    var  c = [mySecond(9, 5), myThird(6, 2)]
-    return c;
+function myFifth (x,y){
+    var  c = [x,y]
+    return c[0] + c[1];
 
 }
-var n = myFifth
-console.log(n[mySecond(9, 5) + myThird(6, 2)])
+var n = 
+console.log(myFifth(1,3))
 /**Q6 ● Write a function that takes an integer minutes and converts it to seconds.*/
-function convert(m,){
-var j;
-j = m * 60;
+function convert(m){
 
-return j;
+return m * 60;
 }
-var con = convert(5)
-console.log(con)
+console.log(convert(5))
 /**Q7 ● Create a function that takes a number as a parameter, increments the number by +1 and returns the result.*/
 function addition(num){
-    var add;
-    add = num + 1
-    return add;
+    return num++;
 
 }
-var additions = addition(5)
-console.log(additions)
+console.log(addition(5))
 /**Q8 ● Write a function that takes the base and height of a triangle and returns its area.*/
 //A = 1/2bh
-function area(b, h){
-    let A;
-A = 1/2 * (b * h)
-return A;
-}
-let Z = area(5, 4)
-console.log(Z)
+function area(b, h){ 
+return 1/2 * (b * h);
+} 
+console.log(area(5, 4))
 /**Q9  */
 function animal(ch, c, p,){
-    var ANI;
-ANI = (ch*2 + c*4 + p*4);
-return ANI;
+
+return (ch*2 + c*4 + p*4);
 }
-var AN = animal(2, 4, 4)
-console.log(AN)
+console.log(animal(2, 4, 4))
 /**Q10 -Create a function that takes an array containing only TWO numbers as a parameter and returns a value that is 3 times the first element of the array*/  //what about the rest ?
 function times3(x,y){
  let times3 = [x, y]
 
-return 3*[0];
+return 3*times3[0];
 }
-var Q = times3(2, 3)
-console.log(Q)
+
+console.log(times3(7, 4))
 /** Or */
 // function times3(num1, num2){
 
