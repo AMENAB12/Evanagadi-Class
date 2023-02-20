62c45c3be9527975231349851d02d20ca2f71bec
AMENAB12 <lalatube71@gmail.com>
2023-02-20 01:56:57 -0800

    new change

--- Third Phase/Introduction to Node modules/nodeServerProject/myWebServer.js
+++ Third Phase/Introduction to Node modules/nodeServerProject/myWebServer.js
@@ -1,11 +1,34 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello, world!');
-  res.end();
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.write('Hello, world!');
+//   res.end();
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server listening on port 3000');
+// });
+
+/**  Express */
+
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+
+res.send('Surver is running');
+
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+
+
+
+app.listen(1234, () => {
+
+
+
+  console.log("Request received and processed ");
+  
+  });
