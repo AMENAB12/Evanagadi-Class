b9e1e128cc7886360ce90d03b85e59ec2929afef
AMENAB12 <lalatube71@gmail.com>
2023-03-06 15:44:46 -0800

    new change

---
+++ Third Phase/Apple React Functional Component/react-apple/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './Component/Header/Header'
+import Footer from './Component/Footer/Footer'
+import Alert from './Component/Main/Alert'
+import First from './Component/Main/First'
+import Second from './Component/Main/Second'
+import Third from './Component/Main/Third'
+import Fourth from './Component/Main/Fourth'
+import Fifth from './Component/Main/Fifth'
+import Sixth from './Component/Main/Sixth'
+export default function App() {
+  return (
+    <div>
+
+<Header/>
+<Alert/>
+<First/>
+<Second/>
+<Third/>
+<Fourth/>
+<Fifth/>
+<Sixth/>
+<Footer/>
+
+    </div>
+  )
+}
+
