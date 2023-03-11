148855b62e29cb84eee9940b75ec87c3b8e11331
AMENAB12 <lalatube71@gmail.com>
2023-03-11 15:49:03 -0800

    new change

--- Third Phase/React components (class based components) and props/example/index.js
+++ Third Phase/React components (class based components) and props/example/index.js
@@ -1,16 +1,51 @@
-var chala = {
-name: 'chala',
-age: 8,
-};
-console.log(chala)
+// var chala = {
+// name: 'chala',
+// age: 8,
+// };
+// console.log(chala)
 
-function Persone(name,age){
-this.name = name;
-this.age = age;
+
+class Persone{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+        }
+        student() {
+                return `${this.name} is evangadi student in group ${this.age}`;
+        }
+
+        getName(){
+return this.name;
+    
+        }
+}
+let abe = new Persone("abebe", 33);
+console.log(abe);
+console.log(abe.student());
+
+class Student extends Persone {
+
+    constructor(grade, dep, name, age){
+        super(name, age);
+        this.grade = grade;
+        this.dep = dep;
+    }
 
 }
-var abe = new Persone("abebe", 4)
-var abee = new Persone("chala", 8)
+let student = new Student ("A", "CS", "Abebe", 65)
+console.log(student)
+console.log(student.getName())
 
-console.log(abe)
-console.log(abee)
+
+
+// var abe = new Persone("abebe", 4)
+// var abee = new Persone("chala", 8)
+
+// console.log(abe)
+// console.log(abee)
+// Persone.prototype.batch = "Oct, 2022"
+// Persone.prototype.studentInfo = function() {
+//     return `${this.name} is ${this.batch} evangadi student in group ${this.age}`;
+// }
+
+// console.log(abe.studentInfo());
